ab0e1da3eb906e6eee0ca54f33673506179f33e5
ahmed35m <muhammadmanzoor16@gmail.com>
2016-10-17 12:27:48 -0600

    List class added . This class contain an instance of ListElement le only.

---
+++ src/List.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.format("Program Started\n");
+		ListElement le = new ListElement();
+		System.out.format("List added\n");
+
+		le.setData(5);
+		System.out.format("value set to 5\n");
+		System.out.println(le.getData());
+
+		
+	}
+	public void addElement(ListElement le)
+	{
+		ListElement lae = new ListElement();
+	}
+}
