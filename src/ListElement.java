35f0b0034dbdc60bf0e7155a46aa21d0937ba3ca
ahmed35m <muhammadmanzoor16@gmail.com>
2016-10-17 11:34:37 -0600

    Start of the project. ListElement class added as specified in the assignment document. No additional code added

---
+++ src/ListElement.java
@@ -0,0 +1,28 @@
+
+public class ListElement {
+	
+	private ListElement next;
+	private ListElement previous;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data = 0;
+		this.next= null;
+		this.previous=null;
+	}
+	
+/*	/
+	 @param data
+	 an i n t e g e r to be s t o r ed in the node
+	 @return
+	 None
+
+	publ i c void setData ( i n t data ) f
+	t h i s . data = data ;
+	g
+	publ i c i n t getData ( ) f
+	r e turn t h i s . data ;
+	g
+	*/
+}
