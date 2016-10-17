2c01c380996647ff4f251b30c65bf91de789dc8a
ahmed35m <muhammadmanzoor16@gmail.com>
2016-10-17 12:16:42 -0600

    formatted it nicely

--- src/ListElement.java
+++ src/ListElement.java
@@ -12,17 +12,16 @@ public class ListElement {
 		this.previous=null;
 	}
 	
-/*	/
-	 @param data
-	 an i n t e g e r to be s t o r ed in the node
-	 @return
-	 None
 
-	publ i c void setData ( i n t data ) f
-	t h i s . data = data ;
-	g
-	publ i c i n t getData ( ) f
-	r e turn t h i s . data ;
-	g
-	*/
+	public void setData (int data)
+	{
+	this.data=data;
+	}
+	
+	public int getData()
+	{
+	
+	return this.data;
+	}
+	
 }
